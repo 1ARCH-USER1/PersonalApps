5d07b1d32def1e8713465ce749debf049f1b3db3
1ARCH-USER1 <157191135+1ARCH-USER1@users.noreply.github.com>
2024-05-02 14:00:41 +0545

    Add files via upload

---
+++ lol.sh
@@ -0,0 +1,13 @@
+sudo pacman -U legacylauncher-1.0-1-any.pkg.tar.zst
+sudo tar -xf Bibata-Original-Classic
+sudo cp -r Bibata-Original-Classic /usr/share/icons/
+sudo unzip cmpack_1.8.8_2.3.2.zip
+sudo cd /home/
+sudo mkdir .minecraft
+sudo cd .minecraft
+sudo mkdir home
+sudo cd
+sudo cd PersonalApps
+sudo cp -r cmpack_1.8.8_2.3.2/cmpack_1.8.8 ~/.minecraft/home/
+clear
+
